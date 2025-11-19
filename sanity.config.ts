1863cab9e52869359ed78f8b6498eceb668560f1
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-11-19 14:21:44 +0100

    refactor: edit

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ import { schemaTypes } from './sanity/schemaTypes';
 export default defineConfig({
   name: 'default',
   title: 'ACOB Lighting Blog',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'x16t7huo',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
   plugins: [
     structureTool({
